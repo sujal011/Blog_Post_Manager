1ef46cde94ab1875d033740288a635c4b1faae42
ShrenaShah <shrenashah7@gmail.com>
2025-07-09 00:52:37 +0530

    add comments feature

--- routes/blog.js
+++ routes/blog.js
@@ -2,8 +2,8 @@ const { Router } = require("express");
 const multer = require("multer");
 const path = require("path");
 const router = Router();
+const Comment = require("../models/comment");
 const { Blog } = require("../models/blog");
-const { populate } = require("../models/user");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -24,9 +24,13 @@ router.get("/add_new", (req, res) => {
 
 router.get("/:id", async (req, res) => {
   const blog = await Blog.findById(req.params.id).populate("createdBy");
+  const comments = await Comment.find({ blogId: req.params.id }).populate(
+    "createdBy"
+  );
   return res.render("blog", {
     user: req.user,
     blog,
+    comments,
   });
 });
 
