75de5392e8caca9860a998e525197a518f0b2581
Sujal Bhavsar <sujalofficial011@gmail.com>
2025-07-11 00:36:47 +0530

    generate with ai feature added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const express = require("express");
 const mongoose = require("mongoose");
+require("dotenv").config();
 const cookieParser = require("cookie-parser");
 const userRoute = require("./routes/user");
 const blogRoute = require("./routes/blog");
@@ -12,13 +13,14 @@ const app = express();
 const PORT = 8000;
 
 mongoose
-  .connect("mongodb://localhost:27017/blog_post_manager")
+  .connect(process.env.MONGODB_URI || "mongodb://localhost:27017/blog-post-manager")
   .then((e) => console.log("mongodb connected"));
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(checkForAuthenticationCookie("token"));
 app.use(express.static(path.resolve("./public")));
