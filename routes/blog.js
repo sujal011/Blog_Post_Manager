bdfb0d4ab63b736de064930fe1e790eb1ff7ffa6
ShrenaShah <shrenashah7@gmail.com>
2025-07-10 18:40:10 +0530

    add delete option to blog deletion

--- routes/blog.js
+++ routes/blog.js
@@ -46,4 +46,25 @@ router.post("/", upload.single("coverImage"), async (req, res) => {
   return res.redirect(`/blog/${blog._id}`);
 });
 
+router.delete("/:id", async (req, res) => {
+  try {
+    const blog = await Blog.findById(req.params.id);
+    if (!blog) {
+      return res.status(404).json({ message: "Blog not found" });
+    }
+
+    if (blog.createdBy.toString() !== req.user._id.toString()) {
+      return res.status(403).json({ message: "Unauthorized" });
+    }
+    await Blog.findByIdAndDelete(req.params.id);
+
+    await Comment.deleteMany({ blogId: req.params.id });
+    return res.status(200).json({ message: "Blog deleted successfully" });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Server error", error: error.message });
+  }
+});
+
 module.exports = router;
